9601b18e9906f140a3a959093de44b93f706c7e3
Roger-Rys <roger.reyesm97@gmail.com>
2022-04-28 22:47:27 -0500

    importar libreria 'mapbox'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoicm9nZXJyeWVzOTAiLCJhIjoiY2wyanVnaml3MDc4YzNjbXhucXVqdXJyZCJ9.1ea20vFsfSTenI6x0AxoYg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1Ijoicm9nZXJyeWVzOTAiLCJhIjoiY2wyanVnaml3MDc4YzNjbXhucXVqdXJyZCJ9.1ea20vFsfSTenI6x0AxoYg'
 };
 
 /*
