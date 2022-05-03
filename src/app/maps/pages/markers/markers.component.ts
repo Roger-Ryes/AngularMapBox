c675f513d273d33d69459887255bba2465e1f6db
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-02 22:45:57 -0500

    Agregar un marcador mapbox

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
-  styles: [
-  ]
+  styles: [`
+   .mapContainer {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+      }
+  `]
 })
-export class MarkersComponent implements OnInit {
+export class MarkersComponent implements AfterViewInit {
+
+  @ViewChild('mapMarker') mapMarker!: ElementRef;
+  center: [number, number] = [-78.48124797222665, -0.17664900227245092];
+
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+        const map = new mapboxgl.Map({
+        container: this.mapMarker.nativeElement,
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: this.center,
+        zoom: 3
+      });
+  
+      const marker = new mapboxgl.Marker({
+        color: "#333",
+        draggable: true
+      })
+      .setLngLat(this.center)
+      .addTo(map);
   }
 
 }
