a698a4102d17b85db14a7832f4ac18e0dd4d3f69
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-03 23:33:51 -0500

    Propiedades

--- src/app/maps/components/min-map/min-map.component.ts
+++ src/app/maps/components/min-map/min-map.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-min-map',
   templateUrl: './min-map.component.html',
-  styles: [
-  ]
+  styles: [`
+    div{
+      width: 100%;
+      height: 150px;
+      margin: 0px;
+    }
+  `]
 })
-export class MinMapComponent implements OnInit {
+export class MinMapComponent implements AfterViewInit {
+  
+  @Input() lnglat: [number, number]=[0,0];
+  @ViewChild("map") divMap!: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lnglat,
+      zoom: 15,
+      interactive: false
+    })
+    new mapboxgl.Marker()
+    .setLngLat(this.lnglat)
+    .addTo(map);
   }
-
 }
